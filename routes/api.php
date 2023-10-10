27b0169e4e653191763ebef4fde837d1450b798f
Miguel Angel Izquierdo <izquierdomiguela@gmail.com>
2023-10-10 16:33:51 +0200

    Add middleware to restaurant routes & refactor api.php

--- routes/api.php
+++ routes/api.php
@@ -9,33 +9,24 @@ use App\Http\Controllers\UserController;
 |--------------------------------------------------------------------------
 | API Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
 */
-
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
 
 // Restaurants
 Route::prefix('restaurants')->group(function () {
     Route::get('/', 'App\Http\Controllers\RestaurantController@index');
-    Route::post('/', 'App\Http\Controllers\RestaurantController@store');
+    Route::post('/', [RestaurantController::class, 'store'])->middleware(['custom-auth']);
     Route::get('/{id}', 'App\Http\Controllers\RestaurantController@show');
-    Route::patch('/{id}', 'App\Http\Controllers\RestaurantController@update');
+    Route::patch('/{id}', [RestaurantController::class, 'update'])->middleware(['custom-auth']);
     Route::delete('/{id}', [RestaurantController::class, 'destroy'])->middleware(['custom-auth']);
 });
 
 //Users
 
 Route::prefix('users')->group(function () {
-    Route::get('/', 'App\Http\Controllers\UserController@index'); // Endpoint para obtener todos los usuarios
-    Route::get('/{id}', 'App\Http\Controllers\UserController@show'); // Endpoint para obtener un usuario por ID
+    Route::get('/', 'App\Http\Controllers\UserController@index'); 
+    Route::get('/{id}', 'App\Http\Controllers\UserController@show'); 
     Route::post('/login', 'App\Http\Controllers\UserController@login');
-    Route::post('/', 'App\Http\Controllers\UserController@store'); // Endpoint para crear un nuevo usuario
-    Route::patch('/{id}', 'App\Http\Controllers\UserController@update'); // Endpoint para actualizar un usuario
-    Route::delete('/{id}', 'App\Http\Controllers\UserController@destroy'); // Endpoint para eliminar un usuario
+    Route::post('/', 'App\Http\Controllers\UserController@store'); 
+    Route::patch('/{id}', 'App\Http\Controllers\UserController@update'); 
+    Route::delete('/{id}', 'App\Http\Controllers\UserController@destroy'); 
 });
